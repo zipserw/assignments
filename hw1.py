5c61203697292d011352fbd0290b3272fba9c650
Krzysztof Zdulski <krzys.zdulski@gmail.com>
2020-03-15 23:03:55 +0100

    Create first task of the hw1 assignment

---
+++ hw1.py
@@ -0,0 +1,16 @@
+# Homework number 1
+
+def get_poland_cases_by_date(day: int, month: int) -> int:
+    """
+    Loads data from
+    https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data
+    /csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+    and returns cases for country 'Poland' given day and month
+
+    :param day: day of month to get the cases for (indexed from 1)
+    :param month: month to get the cases for (indexed from 1)
+    :return: number of cases on a given date
+    """
+    
+    # Type your code here (remove 'pass')
+    pass
